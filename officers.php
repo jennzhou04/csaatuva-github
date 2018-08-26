9134d4f7e0475d06114ace65701d8c0d617b0b0f
Jennifer <jennifer.zhou@thinkingwithtools.com>
2018-08-26 03:51:31 -0400

    added logout

--- officers.php
+++ officers.php
@@ -8,28 +8,6 @@
 $toRoot = './';
 $currentPage = 'officers';
 include($toRoot.'_header.php');
-require('includes/mysqli_connect.php');
-include('chromephp/ChromePhp.php');
-$count = 0;
-$sql = "SELECT * FROM 1718officers";
-$result = mysqli_query($conn, $sql)
-or die("Error: ".mysqli_error($conn));
-ChromePhp::log($result);
-
-// while ($row=mysqli_fetch_array($result)) {
-// 	$id = $row['id']; 
-// 	$name = $row['name']; 
-// 	$year = $row['year'];
-// 	$major = $row['major']; 
-// 	$description = $row['description'];
-// 	$image = $row['image']; 
-// 	$title = $row['title'];
-// 	if ($id > 0) {
-// 		$data[$count] = array($name, $year, $major, $description, $image, $title);
-// 		$count = $count + 1;
-// 	}
-// }
-// ChromePhp::log($data);
 ?>
 		<!-- Main -->
 			<section id="officer">
@@ -42,78 +20,126 @@ ChromePhp::log($result);
 				</div>
 			</section>
 			<?php 
+			require('includes/mysqli_connect.php');
+			$now = date('Y').'-01-01';
+			$count = 0;
+			$sql = "SELECT * FROM officers WHERE (title='President' OR title='Vice President' OR title='Treasurer' OR title='Secretary')
+			AND created > (SELECT year(created) FROM officers GROUP BY created ORDER BY created DESC LIMIT 1)";
+			$result = $conn->query($sql);
+			$exec = [];
+			
+			if ($result->num_rows > 0) {
+				while($row = $result->fetch_assoc()) {
+					if($row['title'] == 'President') {
+                        $p = $row;
+                    } 
+                    if($row['title'] == 'Vice President') {
+                        $vp = $row;
+                    }
+                    if($row['title'] == 'Treasurer') {
+                        $t = $row;
+                    }
+                    if($row['title'] == 'Secretary') {
+                        $s = $row;
+                    }
+					// array_push($officers, $row);
+				}
+			} else {
+				// echo "0 results";
+			}
+			$conn->close();
+			if(!empty($p)) {
+                array_push($exec, $p);
+            }
+            if(!empty($vp)) {
+                array_push($exec, $vp);
+            }
+            if(!empty($t)) {
+                array_push($exec, $t);
+            }
+            if(!empty($s)) {
+                array_push($exec, $s);
+            }
 
-			$EXEC = '<header class="center"><p>Executive Board</p></header><div class="feature-grid">';
-			$community = '<header class="center"><p>Community</p></header><div class="feature-grid">';
-			$culture = '<header class="center"><p>Culture</p></header><div class="feature-grid">';
-			$fundraising = '<header class="center"><p>Fundraising</p></header><div class="feature-grid">';
-			$historic = '<header class="center"><p>Historic</p></header><div class="feature-grid">';
-			$publicity = '<header class="center"><p>Publicity</p></header><div class="feature-grid">';
-			$social = '<header class="center"><p>Social</p></header><div class="feature-grid">';
-			$sports = '<header class="center"><p>Sports</p></header><div class="feature-grid">';
-			$headAdvisors = '<header class="center"><p>Head Advisors</p></header><div class="feature-grid">';
-			$advisors = '<header class="center"><p>Advisors</p></header><div class="feature-grid">';
-			while ($row=mysqli_fetch_array($result)) {
-				if($row['title']=="President") {
-					$pres = '<div class="feature"><div class="image rounded"><img src="';
-					$pres .= $row['image'];
-					$pres .= '" alt="" /></div><div class="content"><header><h4>';
-					$pres .= $row['name'];
-					$pres .= '<span style="font-style:italic;font-size: .8em;font-weight:500;text-transform:none;"> - ';
-					$pres .= $row['year'];
-					$pres .= '</span></h4><p>';
-					$pres .= $row['title'];
-					$pres .= '</p><p>';
-					$pres .= $row['major'];
-					$pres .= '</p></header><p>';
-					$pres .= $row['description'];
-					$pres .= '</o></div></div>';
-				}
-				elseif($row['title']=="Vice President") {
-					$vice = '<div class="feature"><div class="image rounded"><img src="';
-					$vice .= $row['image'];
-					$vice .= '" alt="" /></div><div class="content"><header><h4>';
-					$vice .= $row['name'];
-					$vice .= '<span style="font-style:italic;font-size: .8em;font-weight:500;text-transform:none;"> - ';
-					$vice .= $row['year'];
-					$vice .= '</span></h4><p>';
-					$vice .= $row['title'];
-					$vice .= '</p><p>';
-					$vice .= $row['major'];
-					$vice .= '</p></header><p>';
-					$vice .= $row['description'];
-					$vice .= '</o></div></div>';
-				}
-				elseif($row['title']=="Secretary") {
-					$sec = '<div class="feature"><div class="image rounded"><img src="';
-					$sec .= $row['image'];
-					$sec .= '" alt="" /></div><div class="content"><header><h4>';
-					$sec .= $row['name'];
-					$sec .= '<span style="font-style:italic;font-size: .8em;font-weight:500;text-transform:none;"> - ';
-					$sec .= $row['year'];
-					$sec .= '</span></h4><p>';
-					$sec .= $row['title'];
-					$sec .= '</p><p>';
-					$sec .= $row['major'];
-					$sec .= '</p></header><p>';
-					$sec .= $row['description'];
-					$sec .= '</o></div></div>';
-				}
-				elseif($row['title']=="Treasurer") {
-					$tres = '<div class="feature"><div class="image rounded"><img src="';
-					$tres .= $row['image'];
-					$tres .= '" alt="" /></div><div class="content"><header><h4>';
-					$tres .= $row['name'];
-					$tres .= '<span style="font-style:italic;font-size: .8em;font-weight:500;text-transform:none;"> - ';
-					$tres .= $row['year'];
-					$tres .= '</span></h4><p>';
-					$tres .= $row['title'];
-					$tres .= '</p><p>';
-					$tres .= $row['major'];
-					$tres .= '</p></header><p>';
-					$tres .= $row['description'];
-					$tres .= '</o></div></div>';
-				}
+			echo '<header class="center"><p>Executive Board</p></header><div class="feature-grid">';
+			foreach($exec as $officer) {
+				echo '<div class="feature"><div class="image rounded"><img src="'.$officer['image'].'"></div>';
+				echo '<div class="content"><header><h4>'.$officer['name'].'<span class="small"> - '.$officer['year'].'</span></h4>';
+				echo '<p>'.$officer['title'].'</p><p>'.$officer['major'].'</p></header>';
+				echo '<p>'.$officer['description'].'</p></div></div>';
+			}
+			echo '</div>';
+
+			// $community = '<header class="center"><p>Community</p></header><div class="feature-grid">';
+			// $culture = '<header class="center"><p>Culture</p></header><div class="feature-grid">';
+			// $fundraising = '<header class="center"><p>Fundraising</p></header><div class="feature-grid">';
+			// $historic = '<header class="center"><p>Historic</p></header><div class="feature-grid">';
+			// $publicity = '<header class="center"><p>Publicity</p></header><div class="feature-grid">';
+			// $social = '<header class="center"><p>Social</p></header><div class="feature-grid">';
+			// $sports = '<header class="center"><p>Sports</p></header><div class="feature-grid">';
+			// $headAdvisors = '<header class="center"><p>Head Advisors</p></header><div class="feature-grid">';
+			// $advisors = '<header class="center"><p>Advisors</p></header><div class="feature-grid">';
+			// while ($row=mysqli_fetch_array($result)) {
+			// 	if($row['title']=="President") {
+			// 		$pres = '<div class="feature"><div class="image rounded"><img src="';
+			// 		$pres .= $row['image'];
+			// 		$pres .= '" alt="" /></div><div class="content"><header><h4>';
+			// 		$pres .= $row['name'];
+			// 		$pres .= '<span style="font-style:italic;font-size: .8em;font-weight:500;text-transform:none;"> - ';
+			// 		$pres .= $row['year'];
+			// 		$pres .= '</span></h4><p>';
+			// 		$pres .= $row['title'];
+			// 		$pres .= '</p><p>';
+			// 		$pres .= $row['major'];
+			// 		$pres .= '</p></header><p>';
+			// 		$pres .= $row['description'];
+			// 		$pres .= '</o></div></div>';
+			// 	}
+			// 	elseif($row['title']=="Vice President") {
+			// 		$vice = '<div class="feature"><div class="image rounded"><img src="';
+			// 		$vice .= $row['image'];
+			// 		$vice .= '" alt="" /></div><div class="content"><header><h4>';
+			// 		$vice .= $row['name'];
+			// 		$vice .= '<span style="font-style:italic;font-size: .8em;font-weight:500;text-transform:none;"> - ';
+			// 		$vice .= $row['year'];
+			// 		$vice .= '</span></h4><p>';
+			// 		$vice .= $row['title'];
+			// 		$vice .= '</p><p>';
+			// 		$vice .= $row['major'];
+			// 		$vice .= '</p></header><p>';
+			// 		$vice .= $row['description'];
+			// 		$vice .= '</o></div></div>';
+			// 	}
+			// 	elseif($row['title']=="Secretary") {
+			// 		$sec = '<div class="feature"><div class="image rounded"><img src="';
+			// 		$sec .= $row['image'];
+			// 		$sec .= '" alt="" /></div><div class="content"><header><h4>';
+			// 		$sec .= $row['name'];
+			// 		$sec .= '<span style="font-style:italic;font-size: .8em;font-weight:500;text-transform:none;"> - ';
+			// 		$sec .= $row['year'];
+			// 		$sec .= '</span></h4><p>';
+			// 		$sec .= $row['title'];
+			// 		$sec .= '</p><p>';
+			// 		$sec .= $row['major'];
+			// 		$sec .= '</p></header><p>';
+			// 		$sec .= $row['description'];
+			// 		$sec .= '</o></div></div>';
+			// 	}
+			// 	elseif($row['title']=="Treasurer") {
+			// 		$tres = '<div class="feature"><div class="image rounded"><img src="';
+			// 		$tres .= $row['image'];
+			// 		$tres .= '" alt="" /></div><div class="content"><header><h4>';
+			// 		$tres .= $row['name'];
+			// 		$tres .= '<span style="font-style:italic;font-size: .8em;font-weight:500;text-transform:none;"> - ';
+			// 		$tres .= $row['year'];
+			// 		$tres .= '</span></h4><p>';
+			// 		$tres .= $row['title'];
+			// 		$tres .= '</p><p>';
+			// 		$tres .= $row['major'];
+			// 		$tres .= '</p></header><p>';
+			// 		$tres .= $row['description'];
+			// 		$tres .= '</o></div></div>';
+			// 	}
 				// $id = $row['id']; 
 				// $name = $row['name']; 
 				// $year = $row['year'];
@@ -125,9 +151,27 @@ ChromePhp::log($result);
 				// 	$data[$count] = array($name, $year, $major, $description, $image, $title);
 				// 	$count = $count + 1;
 				// }
+			
+			// $EXEC .= $pres . $vice . $sec . $tres . '</div>';
+			?>
+
+			<?php
+			require('includes/mysqli_connect.php');
+			$now = date('Y').'-01-01';
+			$count = 0;
+			$sql = "SELECT * FROM officers WHERE (title='President' OR title='Vice President' OR title='Treasurer' OR title='Secretary')
+			AND created > (SELECT year(created) FROM officers GROUP BY created ORDER BY created DESC LIMIT 1)";
+			$result = $conn->query($sql);
+			$officers = [];
+			
+			if ($result->num_rows > 0) {
+				while($row = $result->fetch_assoc()) {
+					array_push($officers, $row);
+				}
+			} else {
+				// echo "0 results";
 			}
-			$EXEC .= $pres . $vice . $sec . $tres . '</div>';
-			echo $EXEC;
+			$conn->close();
 			?>
 			<header class="center"><p>Executive Board</p></header>
 			<div class="officer-grid">
